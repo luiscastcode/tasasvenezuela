8eb888d9f905afe5d7cfd13caafd56e436cf2437
Luis Castillo <cncompusistema@gmail.com>
2025-12-27 20:26:58 -0400

    agregar analutycs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,11 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter: vercel(),
+  output: 'server',
+  adapter: vercel(),
+  integrations: [sitemap()],
 });
